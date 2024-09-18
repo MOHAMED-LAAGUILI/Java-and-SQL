89903c3905114be4fa9e00d86566c5042fcc4daa
MOHAMED LAAGUILI <94993660+MOHAMED-LAAGUILI@users.noreply.github.com>
2024-09-18 22:10:37 +0100

    Add files via upload

---
+++ Java Interface graphic Manuel Files (16)/src/package_file/Class_File.java
@@ -0,0 +1,238 @@
+package package_file;
+import java.io.*;
+
+
+
+public class Class_File extends javax.swing.JFrame {
+    public Class_File() {
+        initComponents();
+    }
+    
+     File f;
+    FileOutputStream fw;
+    FileInputStream fr;
+    
+    void Nouveau(){
+        txta.setText(null);
+        txta.requestFocus();
+    }
+    
+    void Ajouter() throws IOException{
+        byte [] tb = new byte[512];
+        f = new File("D:\\Mohamed");
+        if(!f.exists())
+            f.mkdir();
+        tb = (txta.getText() + "\n").getBytes();
+          File f1 = new File(f,"test.txt");
+        fw = new FileOutputStream(f1,true);
+        fw.write(tb);
+        fw.close(); 
+    }
+    
+    String Afficher() throws IOException{
+        int byt = 0;
+        String message = "";
+        f = new File("D:\\Mohamed\\test.txt");
+       fr = new FileInputStream(f);
+       while((byt = fr.read()) != -1){
+                message += (char) byt;
+       }
+       fr.close();
+       return message;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txta = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtc = new javax.swing.JTextArea();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        btn = new javax.swing.JButton();
+        bta = new javax.swing.JButton();
+        btf = new javax.swing.JButton();
+        btf1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtaActionPerformed(evt);
+            }
+        });
+
+        txtc.setColumns(20);
+        txtc.setRows(5);
+        jScrollPane1.setViewportView(txtc);
+
+        jLabel1.setText("Text a ajouter");
+
+        jLabel2.setText("Afficher contenu");
+
+        btn.setText("Nouveau");
+        btn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnActionPerformed(evt);
+            }
+        });
+
+        bta.setText("Ajouter");
+        bta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btaActionPerformed(evt);
+            }
+        });
+
+        btf.setText("Fermer");
+        btf.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btfActionPerformed(evt);
+            }
+        });
+
+        btf1.setText("Afficher");
+        btf1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btf1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jLabel1)
+                        .addGroup(layout.createSequentialGroup()
+                            .addComponent(btn)
+                            .addGap(28, 28, 28)
+                            .addComponent(bta)
+                            .addGap(29, 29, 29)
+                            .addComponent(btf))
+                        .addComponent(txta))
+                    .addComponent(btf1))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jScrollPane1)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 110, Short.MAX_VALUE)
+                        .addComponent(jLabel2)
+                        .addGap(118, 118, 118))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(52, 52, 52)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(txta, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(11, 11, 11)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btn)
+                            .addComponent(bta)
+                            .addComponent(btf))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btf1))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void txtaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtaActionPerformed
+
+    private void btnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActionPerformed
+        Nouveau();
+    }//GEN-LAST:event_btnActionPerformed
+
+    private void btaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btaActionPerformed
+        try {
+            Ajouter();
+            Nouveau();    
+        } catch (IOException ex) {
+             }
+    }//GEN-LAST:event_btaActionPerformed
+
+    private void btfActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btfActionPerformed
+       System.exit(0);
+    }//GEN-LAST:event_btfActionPerformed
+
+    private void btf1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btf1ActionPerformed
+        txtc.setText(null);
+        try {
+            txtc.setText(Afficher());
+        } catch (IOException ex) {
+           }
+    }//GEN-LAST:event_btf1ActionPerformed
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Class_File.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Class_File.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Class_File.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Class_File.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Class_File().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bta;
+    private javax.swing.JButton btf;
+    private javax.swing.JButton btf1;
+    private javax.swing.JButton btn;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField txta;
+    private javax.swing.JTextArea txtc;
+    // End of variables declaration//GEN-END:variables
+
+    private void JOptionPane(Class_File aThis, String il_faut_remplir_la_zone_de_text, String zone_Vide, int ERROR_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
